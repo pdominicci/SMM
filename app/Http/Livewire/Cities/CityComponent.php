771c7ea25d82d0be51bcf6a3dc74a6bb6da417c6
Pablo Dominicci <pablodominicci@gmail.com>
2021-05-26 20:09:22 -0300

    cities everything

---
+++ app/Http/Livewire/Cities/CityComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Cities;
+
+use Livewire\Component;
+
+class CityComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.cities.city-component');
+    }
+}
